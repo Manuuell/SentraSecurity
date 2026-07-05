096de22df1f591073473fc41ff5c83be5156fbaa
Manuel Esteban <estebanmanuel907@gmail.com>
2026-07-05 15:52:46 -0500

    Landing: achicar franja de color debajo del nav

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -70,6 +70,7 @@ export default function LandingPage() {
         <div className="lg-blob lg-blob--navy" />
         <div className="lg-blob lg-blob--cyan-1" />
         <div className="lg-blob lg-blob--cyan-2" />
+        <div className="lg-blob lg-blob--cyan-3" />
       </div>
 
       <nav className="lg-nav">
